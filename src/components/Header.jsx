199c962bbf39af371f47afefcb4d020b170d7473
Gayatri11-tech <gayatridahake11@gmail.com>
2025-12-17 01:32:03 +0530

    UI changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import { Container, Image, Nav, Navbar } from "react-bootstrap";
 import logoimg from "../assets/images/logo-removebg.png";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Header = ({activeSection,scrollToRef,contactusRef, ProductsRef,faqRef,HomeRef,aboutusRef}) => {
-
+const [expanded, setExpanded] = useState(false);
   useEffect(()=>{
   const navbar = document.querySelector(".navbar");
   window.addEventListener("scroll", function () {
@@ -17,19 +17,19 @@ export const Header = ({activeSection,scrollToRef,contactusRef, ProductsRef,faqR
   
   return (
     <>
-      <Navbar expand="lg" className="navbar-header">
+      <Navbar expand="lg"  expanded={expanded} className="navbar-header">
         <Container>
           <Navbar.Brand href="#home">
             <Image src={logoimg} alt="lalu wafers" width={75} height={75} />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" onClick={() => scrollToRef(HomeRef)} className={activeSection==='home'?"active":''}>Home</Nav.Link>
-              <Nav.Link href="#about" onClick={() => scrollToRef(aboutusRef)} className={activeSection==='about'?"active":''}>About</Nav.Link>
-              <Nav.Link href="#product" onClick={() => scrollToRef(ProductsRef)} className={activeSection==='product'?"active":''}>Product</Nav.Link>
-              <Nav.Link href="#faq" onClick={() => scrollToRef(faqRef)} className={activeSection==='faq'?"active":''}>FAQ</Nav.Link>
-              <Nav.Link href="#contact" onClick={() => scrollToRef(contactusRef)} className={activeSection==='contact'?"active":''}>Contact</Nav.Link>
+              <Nav.Link href="#home"  onClick={() => {scrollToRef(HomeRef);setExpanded(false);}} className={activeSection==='home'?"active":''}>Home</Nav.Link>
+              <Nav.Link href="#about" onClick={() => {scrollToRef(aboutusRef);setExpanded(false);}} className={activeSection==='about'?"active":''}>About</Nav.Link>
+              <Nav.Link href="#product" onClick={() => {scrollToRef(ProductsRef);setExpanded(false);}} className={activeSection==='product'?"active":''}>Product</Nav.Link>
+              <Nav.Link href="#faq" onClick={() => {scrollToRef(faqRef);setExpanded(false);}} className={activeSection==='faq'?"active":''}>FAQ</Nav.Link>
+              <Nav.Link href="#contact" onClick={() => {scrollToRef(contactusRef);setExpanded(false);}} className={activeSection==='contact'?"active":''}>Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
